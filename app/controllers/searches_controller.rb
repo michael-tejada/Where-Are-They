1a07b5346f335fa0980155c15205015be19ce98e
rnackman <rachel.nackman@gmail.com>
2015-03-30 18:22:59 -0400

    Refactor models & controllers; add remote: true.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,15 @@
 class SearchesController < ApplicationController
 
   def index
-		@cast_data = Api.new.get_cast_data(params[:movie_title])
+    @search = Search.new
 	end
+
+  def create
+    @cast_data = Search.new.get_cast_data(params[:search][:movie_title])
+    respond_to do |f|
+      f.html
+      f.js
+    end
+  end
 
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -6,5 +6,21 @@ class Search
     false
   end
 
+  attr_accessor :movie_title, :url, :cast_data
 
+  def get_cast_data(movie_title)
+    # clean up the user's input to feed to the URL
+    @movie_title = sanitizeTitle(movie_title)
+    # use movie_title to find the movie by title
+    # @url = "http://api.themoviedb.org/3/search/movie?api_key=#{API_KEY}&query=#{@movie_title}"
+    # @cast_data = JSON.load(open(@url))
+    @cast_data = "hello"
+    # capture the movie's id
+    # find the movie cast by movie id
+    # return the cast data
+  end
+
+  def sanitizeTitle(movie_title)
+    movie_title.strip.downcase.gsub(/ +/, '%20')
+  end
 end
