4687431594679e4c5d2965288f7b5dca55295baf
rnackman <rachel.nackman@gmail.com>
2015-03-30 14:50:14 -0400

    Edit searches controller.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,7 @@
 class SearchesController < ApplicationController
 
 	def index
-		@search = Search.new
-	end
-
-	def create
-		@cast_data = Search.new.get_cast(params[:search][:keyword])
+		@cast_data = Search.new.get_cast(params[:keyword])
 	end
 
 end
