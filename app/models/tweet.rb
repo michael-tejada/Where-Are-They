777803016ea28932e8e143d7c93201f3f3041a4c
rnackman <rachel.nackman@gmail.com>
2015-03-31 14:41:12 -0400

    Add Twitter gem & retrieval of user timeline.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,23 +6,21 @@ class Tweet
     false
   end
 
+  
+
   def get_twitter_feed(actor_name)
-    #take the actor name
-    #clean it up
-    @actor_name = sanitize_name(actor_name)
-    #search Twitter to find the user with that name
 
+    client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV['twitter_consumer_key']
+    config.consumer_secret     = ENV['twitter_consumer_secret']
+    config.access_token        = ENV['twitter_access_token']
+    config.access_token_secret = ENV['twitter_access_secret']
+    end
+
+    possible_actors = client.user_search(actor_name)
+    actor_screen_name = possible_actors.select {|a| a.verified? == true}.first
     
-    binding.pry
-    #get that user's timeline
-    #return it as an embeddable object
-
-    # timeline_url = "https://api.twitter.com/1.1/statuses/user_timeline.json?name=twitterapi"
-    "this works"
-  end
-
-  def sanitize_name(actor_name)
-    actor_name.gsub(' ', '%20')
+    actor_timeline = client.user_timeline(actor_screen_name)
   end
 
 end
