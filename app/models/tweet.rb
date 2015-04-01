73baf925b382feba92663c8e294bf84df46eb16c
rnackman <rachel.nackman@gmail.com>
2015-04-01 16:24:58 -0400

    Quick fix for styling of movie cards.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -32,8 +32,9 @@ class Tweet
     actor_credits_url = "http://api.themoviedb.org/3/person/#{actor_id}/combined_credits?api_key=#{ENV['movie_database_api_key']}"
     json = JSON.load(open(actor_credits_url))
     filmography_data = json['cast'].collect {|m| [m['release_date'] ? m['release_date'][0..3] : "", m['title'] ? m['title'] : "", m['id']]}
+    all_movie_ids = filmography_data.collect {|m| m[2]}
+    binding.pry
     filmography_data = filmography_data.select {|m| m[0] != ""}
     filmography_data.collect {|m| "#{m[0]}: #{m[1]}"}.sort
-    # binding.pry
   end
 end
